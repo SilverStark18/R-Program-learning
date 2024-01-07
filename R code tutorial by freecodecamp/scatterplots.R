b3f0dfcfae7596d09f828f68a3179cb667b42df6
SilverStark18 <amarlearns18215@gmail.com>
2024-01-07 22:07:03 +0530

    added new plot img and code

--- R code tutorial by freecodecamp/scatterplots.R
+++ R code tutorial by freecodecamp/scatterplots.R
@@ -7,3 +7,10 @@ hist(mtcars$wt)
 hist(mtcars$mpg)
 
 plot(mtcars$wt,mtcars$mpg)
+
+plot(mtcars$wt,mtcars$mpg,
+     pch=19,
+     cex=1.5,
+     col="red",
+     main="mpg as a function of weight of cars",
+     xlab="weight (in 1000 pounds)",ylab="mpg")
